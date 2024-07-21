56f89a83ae571ebcb231b1824ee65d794fb8e13a
Mohan H S <138346530+Mohanmanuhs@users.noreply.github.com>
2024-07-21 18:57:12 +0530

    Update BankUserDetails.java

--- SecurityDB/src/main/java/com/springsecurity/Security/config/BankUserDetails.java
+++ SecurityDB/src/main/java/com/springsecurity/Security/config/BankUserDetails.java
@@ -11,6 +11,11 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 
+
+//we are giving the username and password from functions instead of giving it from application.properties file
+
+PasswordEncoder passwordEncoder = new ByCryptPasswordEncoder();
+
 @Service
 public class BankUserDetails implements UserDetailsService {
     @Override
@@ -19,7 +24,10 @@ public class BankUserDetails implements UserDetailsService {
         //repo.findByUser(user)
         //size>0
         List<GrantedAuthority> authorities1=new ArrayList<>();
+
+        //we first encode the password entered by user and we have to match it with stored encoded value
+        String encodedPass = passwordEncoder.encode("test"); 
         authorities1.add(new SimpleGrantedAuthority("ADMIN"));
-        return new User("test","test",authorities1);
+        return new User("test",encodedPass,authorities1);
     }
 }
